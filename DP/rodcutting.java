f3aea3bae8d8c11cdd0dca6a1b2cbf6f9ee8a4f5
Gaurav Singh <71409257+firsty-first@users.noreply.github.com>
2024-03-26 10:46:40 +0530

    Update rodcutting.java

--- DP/rodcutting.java
+++ DP/rodcutting.java
@@ -7,18 +7,17 @@ class Solution{
         for(int i=0;i<n;i++)
         Arrays.fill(dp[i],-1);
         
-        return f(0,n,price,dp);
+        return f(n-1,n,price,dp);
     }
     static int f(int ind,int n,int price[],int [][]dp)
     {
-        
-        if(ind==price.length)
+        if(ind==0)
         {
           return n*price[0];
         }
         if(dp[ind][n]!=-1)
         return dp[ind][n];
-        int not=f(ind+1,n,price,dp);
+        int not=f(ind-1,n,price,dp);
         int cut=Integer.MIN_VALUE;
         if(n-(ind+1)>=0)
         cut=price[ind]+f(ind,n-(ind+1),price,dp);
