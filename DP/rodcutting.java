a8e0c93fbaccff6fdeea4cb6cedee5480f61b395
Gaurav Singh <71409257+firsty-first@users.noreply.github.com>
2024-03-26 09:22:45 +0530

    base case

---
+++ DP/rodcutting.java
@@ -0,0 +1,27 @@
+
+
+class Solution{
+    public int cutRod(int price[], int n) {
+        //code here
+        int dp[][]=new int [n][n+1];
+        for(int i=0;i<n;i++)
+        Arrays.fill(dp[i],-1);
+        
+        return f(0,n,price,dp);
+    }
+    static int f(int ind,int n,int price[],int [][]dp)
+    {
+        
+        if(ind==price.length)
+        {
+          return n*price[0];
+        }
+        if(dp[ind][n]!=-1)
+        return dp[ind][n];
+        int not=f(ind+1,n,price,dp);
+        int cut=Integer.MIN_VALUE;
+        if(n-(ind+1)>=0)
+        cut=price[ind]+f(ind,n-(ind+1),price,dp);
+        return dp[ind][n]=Math.max(cut,not);
+    }
+}
