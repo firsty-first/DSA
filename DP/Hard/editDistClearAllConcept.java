db87e1a2a6af04305816f2a029b09493d153bdc4
Gaurav Singh <71409257+firsty-first@users.noreply.github.com>
2024-03-27 12:03:46 +0530

    Update editDistClearAllConcept.java

--- DP/Hard/editDistClearAllConcept.java
+++ DP/Hard/editDistClearAllConcept.java
@@ -6,8 +6,29 @@ class Solution {
         int m=t.length();
         int dp[][]=new int[n+1][m+1];
         for(int i=0;i<n+1;i++)
-        Arrays.fill(dp[i],-1);
-        return f(n-1,m-1,s,t,dp);
+        Arrays.fill(dp[i],0); // for tabulation
+        //Arrays.fill(dp[i],-1);  for rec
+        for(int i=0;i<=n;i++)
+        dp[i][0]=i;
+        for(int j=0;j<=m;j++)
+        dp[0][j]=j;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s.charAt(i-1)==t.charAt(j-1))
+                dp[i][j]=dp[i-1][j-1];
+                else
+                {
+                    int del=1+dp[i-1][j];
+                    int ins=1+dp[i][j-1];
+                    int repl=1+dp[i-1][j-1];
+                    dp[i][j]=Math.min(Math.min(del,ins),repl);
+                }
+            }
+        }
+        return dp[n][m];
+        //return f(n-1,m-1,s,t,dp);
     }
     static int f(int i,int j,String s, String t,int dp[][])
     {
