96cfcee13624322410e92a5ea51476456663a69f
Gaurav Singh <71409257+firsty-first@users.noreply.github.com>
2024-04-13 20:30:09 +0530

    Update nextGreaterEle.java

--- Array/nextGreaterEle.java
+++ Array/nextGreaterEle.java
@@ -1,22 +1,27 @@
-class Solution {
-    public int[] nextGreaterElements(int[] arr) 
-    {
-        int n=arr.length;
-     int ans[]=new int[n];
-        Stack<Integer> stk=new Stack<>();
+
+
+class Solution
+{
+    //Function to find the next greater element for each element of the array.
+    public static long[] nextLargerElement(long[] arr, int n)
+    { 
+        long ans[]=new long[n];
+        Stack<Long> stk=new Stack<>();
         // Your code here
-        for(int i=2*n-1;i>=0;i--)
+        for(int i=n-1;i>=0;i--)
         {
-            while(stk.isEmpty()==false && stk.peek()<=arr[i%n])
+            while(stk.isEmpty()==false && stk.peek()<=arr[i])
             {
                 stk.pop();
             }
            if(stk.isEmpty()==false)
-           ans[i%n]=stk.peek();
+           ans[i]=stk.peek();
            else
-           ans[i%n]=-1;
-           stk.push(arr[i%n]);
+           ans[i]=-1;
+           stk.push(arr[i]);
         }
-        return ans;   
-    }
+        return ans;
+    } 
 }
+//TC:O(2N) bcz stack while loop will run for maximum of N times
+//brute force o(N^2) in worst case
